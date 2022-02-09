58b038eee5ac523193ec0c91dbc81a7e659c3f97
GC <2845740149@qq.com>
2022-02-09 21:17:34 +0800

    Review the knowledge and fix some bugs

--- JavaScript Mosh/Basic/1-Basic.js
+++ JavaScript Mosh/Basic/1-Basic.js
@@ -1,15 +1,15 @@
 /*
  * @Date: 2021-11-20 18:38:37
  * @LastEditors: GC
- * @LastEditTime: 2022-01-07 21:15:09
- * @FilePath: \JavaScrip Mosh\Basic\1-Basic.js
+ * @LastEditTime: 2022-02-03 23:17:33
+ * @FilePath: \JavaScript Mosh\Basic\1-Basic.js
  */
 
 // 1: Variables
-let name = 'John Fish'
-console.log(name)
+let name = "John Fish";
+console.log(name);
 
-//  Some rules of define the variable
+//  Some rules of defining the variable
 //      1: Cannot be a reserved keyword
 //      2: Should be meaningful
 //      3: Cannot start with a number
@@ -20,15 +20,8 @@ console.log(name)
 let firstName = "John";
 let lastName = "Fish";
 
-// The first letter of the first word should be lowercase and the first letter of every word after should be uppercase, this is 
+// The first letter of the first word should be lowercase and the first letter of every word after should be uppercase, this is
 //      what we called camel notation. --> firstName
-
-
-
-
-
-
-
 
 // 2: Constants
 let interestRate = 0.2;
@@ -42,74 +35,54 @@ interestRate = 1;
 console.log(interestRate);
 // From the result, we will get an error message
 
-
-
-
-
-
-
 // 3: Primitive types (String, Number, Boolean, undefined, null)
-let name = "John";         // String literal
-let age = 19;              // Number literal
-let isApprove = true;      // Boolean literal
+let name = "John"; // String literal
+let age = 19; // Number literal
+let isApprove = true; // Boolean literal
 let firstName = undefined; // Undefined
 
 // Clear the value of a variable
-let lastName = null // Null
-
-
-
-
-
+let lastName = null; // Null
 
 // 4: Dynamic Typing
-let name = 'John';
-let firstName = undefined
+let name = "John";
+let firstName = undefined;
 // Then go back to the console in the browser, and we type:
 typeof name;
 name = 1;
-typeof name; // Now the type is changed 
+typeof name; // Now the type is changed
 
-
-typeof firstName // We have known that "undefined" is the primitive type, but this demo we set the firstName to undefined,
-                 // it is a value too.
-
-
-
+typeof firstName; // We have known that "undefined" is the primitive type, but this demo we set the firstName to undefined,
+// it is a value too.
 
 // 5: Objects
 //      We know the primitive types before, and we also have reference types(objects, function, array)
 
 // Define a person object:
 let person = {
-    name: 'Tim',
-    age: 23 
-}
+  name: "Tim",
+  age: 23,
+};
 
 console.log(person);
-
 
 // Change the name of this person:
 //   There are two ways to work with properties:
 
 // 1: Dot Notation:
-person.name = 'Mosh';
+person.name = "Mosh";
 console.log(person.name);
 
 // 2: Bracket Notation:
-person['name'] = 'Marry';
-console.log(person['name']);
-
-
-
+person["name"] = "Marry";
+console.log(person["name"]);
 
 // 6: Arrays
-let selectedColors = ['red', 'blue'];
+let selectedColors = ["red", "blue"];
 
 // Because it is a dynamic language, so this line of code means the third element in this array is green.
 //   The length is dynamic, it can be changed
-selectedColors[2] = 'green';
-
+selectedColors[2] = "green";
 
 // And in JavaScript we can store different types in an array.
 selectedColors[3] = 1;
@@ -118,45 +91,35 @@ console.log(selectedColors);
 // Access the element with the index
 console.log[selectedColors[0]];
 
-
 // On the console:
 typeof selectedColors; // Array is an object in JavaScript, so we can access the properties using dot notation.
 
-
 // It returns the number of items or elements in an array.
-console.log(selectedColors.length)
-
-
+console.log(selectedColors.length);
 
 // 7: Functions
 
 // Parameter
 function greet(name, lastName) {
-    console.log("Hello " + name + ' ' + lastName);
+  console.log("Hello " + name + " " + lastName);
 }
 
-
 // Argument is the actual value with supply for that parameter
-greet('John', 'Fish');
-greet('Mosh');
-
-
-
-
+greet("John", "Fish");
+greet("Mosh");
 
 // 8: Types of functions
 
 // Performing a task
 function greet(name, lastName) {
-    console.log("Hello " + name + ' ' + lastName);
+  console.log("Hello " + name + " " + lastName);
 }
 
 // Calculating a value
 function square(number) {
-    return number * number;
+  return number * number;
 }
 
 let number = square(3);
 console.log(number);
 console.log(square(5));
-
--- JavaScript Mosh/Basic/3-Flow_Control.js
+++ JavaScript Mosh/Basic/3-Flow_Control.js
@@ -1,206 +1,148 @@
 /*
  * @Date: 2021-11-21 18:45:12
  * @LastEditors: GC
- * @LastEditTime: 2022-01-09 20:36:58
- * @FilePath: \JavaScrip Mosh\Basic\3-Flow_Control.js
+ * @LastEditTime: 2022-02-05 22:17:54
+ * @FilePath: \JavaScript Mosh\Basic\3-Flow_Control.js
  */
-
 
 // 1: If...else
 
 let hour = 10;
-
-if (hour >= 6 && hour < 12) 
-    console.log("Good morning!");
-
-else if (hour >=12 && hour < 18) 
-    console.log("Good afternoon!");
-
-else
-    console.log("Good evening!");
-
-
-
-
+if (hour >= 6 && hour < 12) console.log("Good morning!");
+else if (hour >= 12 && hour < 18) console.log("Good afternoon!");
+else console.log("Good evening!");
 
 // 2: Switch...Case
-let role = 'guest';
+let role = "guest";
 switch (role) {
-    case 'guest':
-        console.log('Guest User');
-        break;
-        
-    case 'Moderator':
-        console.log('Moderator User');
-        break;
+  case "guest":
+    console.log("Guest User");
+    break;
 
-    default:
-        console.log('Unknown User');
-        // We do not need 'break' this time.
-} 
+  case "Moderator":
+    console.log("Moderator User");
+    break;
 
-
-
-
-
+  default:
+    console.log("Unknown User");
+    // We do not need 'break' this time.
+}
 
 // 3: For
 
 for (let i = 0; i < 5; i++) {
-    console.log('Hello World', i);
+  console.log("Hello World", i);
 
-    if (i % 2 !== 0) 
-        console.log(i);
+  if (i % 2 !== 0) console.log(i);
 }
-
 
 for (let x = 5; x >= 1; x--) {
-    console.log(x);
+  console.log(x);
 }
-
-
-
-
-
-
 
 // 4: While
 
 let i = 0;
 while (i <= 5) {
-    if (i % 2 !== 0)
-        console.log(i);
-    i++; 
+  if (i % 2 !== 0) console.log(i);
+  i++;
 }
-
-
-
 
 // 5: Do...While
 let x = 0;
 do {
-    if (x % 2 !== 0) console.log(i);
-    i++;
+  if (x % 2 !== 0) console.log(i);
+  i++;
 } while (x <= 5);
-
-
-
-
 
 // 6: Infinite Loops:
 let i = 0;
 while (i < 5) {
-    console.log(i);
-    // i++;
+  console.log(i);
+  // i++;
 }
-
-
 
 while (true) {
-    console.log('Hello World');
+  console.log("Hello World");
 }
-
 
 let y = 0;
 do {
-    console.log(y);
-    y++;
+  console.log(y);
+  y++;
 } while (y < 5);
-
-
-
-
-
 
 // 7: For...in
 
 const person = {
-    name: 'Mosh',
-    age: 39
-}
+  name: "Mosh",
+  age: 39,
+};
 
-for(let key in person) {
-    console.log(key);
-    console.log(key, person[key]);
+for (let key in person) {
+  console.log(key);
+  console.log(key, person[key]);
 }
 
 // Another example:
-colors = ['red', 'blue', 'green'];
+colors = ["red", "blue", "green"];
 for (let index in colors) {
-    console.log(index);
-    console.log(index, colors[index]);
+  console.log(index);
+  console.log(index, colors[index]);
 }
-
-
-
 
 // 8: For...of
 
-colors = ['red', 'blue', 'green'];
-for (let color of colors) 
-    console.log(color)
+colors = ["red", "blue", "green"];
+for (let color of colors) console.log(color);
 
 // color represents the element of this array.
 // So we use the for-in loop to iterate over the properties of an object, and for-of loop to iterate over the elements or items
 //      in an array.
 
-
-
-
-
-
 // 9: Break and continue
 
 let i = 0;
 while (i < 10) {
-    // if (i === 5) 
-    //     break;
-    // Jump out of the loop
+  // if (i === 5)
+  //     break;
+  // Jump out of the loop
 
-
-    if (i % 2 === 0) {
-        i++;
-        continue;
-        // When JavaScript see this key word, it will jump to the beginning of the loop and continue the next execution of 
-        //      the next iteration.
-    }
-
-    console.log(i);
+  if (i % 2 === 0) {
     i++;
+    continue;
+    // When JavaScript see this key word, it will jump to the beginning of the loop and continue the next execution of
+    //      the next iteration.
+  }
+
+  console.log(i);
+  i++;
 }
 
-
-
-    
 // 10: Exercise:
 
 // (1):
 function max(x, y) {
-    if (x > y) return x;
-    return y;
+  if (x > y) return x;
+  return y;
 
-    // return (a > b) ? a : b;
+  // return (a > b) ? a : b;
 }
 
 let number = max(1, 3);
 console.log(number);
 
-
-
-// (2): 
+// (2):
 function isLandScope(width, height) {
-    if (width > height) return width;
-    return height;
+  if (width > height) return width;
+  return height;
 
-    // return (width > height) ? true : false;
+  // return (width > height) ? true : false;
 
-    // return (width > height);
+  // return (width > height);
 }
 
 console.log(isLandScope(800, 600));
-
-
-
 
 // (3):
 
@@ -215,114 +157,95 @@ const output = fizzBuzz(55);
 console.log(output);
 
 function fizzBuzz(input) {
-    if (typeof input !== 'number') return Nan; // Nan --> Not a number
-    else {
-        if ((input % 3 === 0) && (input % 5 === 0)) return 'FizzBuzz';
-        if (input % 3 === 0) return 'Fizz';
-        if (input % 5 === 0) return 'Buzz';
-        return input; 
-    }
+  if (typeof input !== "number") return Nan;
+  // Nan --> Not a number
+  else {
+    if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz";
+    if (input % 3 === 0) return "Fizz";
+    if (input % 5 === 0) return "Buzz";
+    return input;
+  }
 }
 
-
-
-
-
-// 4: 
+// 4:
 
 // Speed Limit = 70
 // 5 km/h --> 1 point
 // Math.floor(1.3) --> 1
 // More than 12 points (180 km/h) --> Suspended
 
-checkSpeed(80)
+checkSpeed(80);
 
 function checkSpeed(speed) {
-    const speedLimit = 70;
-    const kmPerPoint = 5;
-    if (speed < speedLimit + kmPerPoint) {
-        console.log('Okkkkk');
-        return;
-        // We will jump out of this function, and the code after won't be execute.
-    }
-    const points = Math.floor((speed - speedLimit) / kmPerPoint);
-    if (points >= 12)
-        console.log('License suspended.');
-    else
-        console.log('Points: ', points);
+  const speedLimit = 70;
+  const kmPerPoint = 5;
+
+  if (speed < speedLimit + kmPerPoint) {
+    console.log("Okkkkk");
+    return;
+    // We will jump out of this function, and the code after won't be execute.
+  }
+
+  const points = Math.floor((speed - speedLimit) / kmPerPoint);
+
+  if (points >= 12) console.log("License suspended.");
+  else console.log("Points: ", points);
 }
-
-
-
 
 // 5:
 showNumber(10);
 
 function showNumber(limit) {
-    for (let i = 0; i <= 10; i++) {
-        if (limit % 2 === 0) console.log(i, 'EVEN');
-        console.log(i, 'ODD');
+  for (let i = 0; i <= 10; i++) {
+    if (limit % 2 === 0) console.log(i, "EVEN");
+    console.log(i, "ODD");
 
-        // const message = (limit % 2 === 0) ? 'EVEN' : 'ODD';
-        // console.log(i, message);
-        
-    }
+    // const message = (limit % 2 === 0) ? 'EVEN' : 'ODD';
+    // console.log(i, message);
+  }
 }
-   
 
-
-// 6: 
-const array = [1, 0, 2, 4, undefined, '', null];
+// 6:
+const array = [1, 0, 2, 4, undefined, "", null];
 countTruthy(array);
 
 function countTruthy(array) {
-    let count = 0;
-    for (let value of array) 
-        if (value) count ++;
-    return count;
+  let count = 0;
+  for (let value of array)
+    if (value) count++;
+  return count;
 }
-
-
-
 
 // 7:
 const movie = {
-    title: 'a',
-    releaseYear: 2020,
-    rating: 3.5,
-    director: 'b'
+  title: "a",
+  releaseYear: 2020,
+  rating: 3.5,
+  director: "b",
 };
-
 
 showProperties(movie);
 
 function showProperties(obj) {
-    for (let key in obj)
-        if (typeof obj[key] === 'string')
-            console.log(key, obj[key]);
+  for (let key in obj)
+    if (typeof obj[key] === "string") console.log(key, obj[key]);
 }
-
-
 
 // 8:
 
 // Multiples of 3: 3, 6, 9
 // Multiples of 5: 5, 10
 
-console.log(sum(10))
+console.log(sum(10));
 
-function sum(limit) { 
-    let sum = 0;
+function sum(limit) {
+  let sum = 0;
 
-    for(let i = 0; i <= limit; i++) 
-        if (i % 3 === 0 || i % 5 === 0)
-            sum += i;
-    
-    return sum;
+  for (let i = 0; i <= limit; i++)
+    if (i % 3 === 0 || i % 5 === 0) sum += i;
+
+  return sum;
 }
-
-
-
 
 // 9:
 
@@ -339,20 +262,17 @@ const array = [80, 80, 50];
 console.log(calculateGrade(array));
 
 function calculateGrade(marks) {
-    let sum = 0;
-    for (let mark of marks)
-        sum += mark;
+  let sum = 0;
+  for (let mark of marks) sum += mark;
 
-    let average = sum / marks.length;
+  let average = sum / marks.length;
 
-    if (average < 60) return 'F';
-    if (average < 70) return 'D';
-    if (average < 80) return 'C';
-    if (average < 90) return 'B';
-    return 'A';
-
+  if (average < 60) return "F";
+  if (average < 70) return "D";
+  if (average < 80) return "C";
+  if (average < 90) return "B";
+  return "A";
 }
-
 
 // Another advanced solution: extract some lines of code and put them into a separated function.
 
@@ -360,41 +280,34 @@ const array = [80, 80, 50];
 console.log(calculateGrade(array));
 
 function calculateGrade(marks) {
-    let average = calculateAverage(marks);
+  let average = calculateAverage(marks);
 
-    if (average < 60) return 'F';
-    if (average < 70) return 'D';
-    if (average < 80) return 'C';
-    if (average < 90) return 'B';
-    return 'A';
+  if (average < 60) return "F";
+  if (average < 70) return "D";
+  if (average < 80) return "C";
+  if (average < 90) return "B";
+  return "A";
 }
 
 function calculateAverage(array) {
-    let sum = 0;
-    for (let value of array)
-        sum += value;
+  let sum = 0;
+  for (let value of array) sum += value;
 
-    return sum / array.length;
+  return sum / array.length;
 }
-
-
-
-
 
 // 10:
 showStars(10);
 
 function showStars(rows) {
   for (let i = 1; i <= rows; i++) {
-    let pattern = '';
+    let pattern = "";
     for (let j = 0; j < i; j++) {
-      pattern += '*';
+      pattern += "*";
     }
     console.log(pattern);
-  }     
+  }
 }
-
-
 
 // 11:
 
@@ -404,32 +317,29 @@ function showStars(rows) {
 showPrime(20);
 
 function showPrime(limit) {
-    for (let number = 2; number <= limit; number++) {
-
-        let isPrime = true;
-        for (let factor = 2; factor < number; factor++) {
-            if (number % factor === 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) console.log(number);
+  for (let number = 2; number <= limit; number++) {
+    let isPrime = true;
+    for (let factor = 2; factor < number; factor++) {
+      if (number % factor === 0) {
+        isPrime = false;
+        break;
+      }
     }
+    if (isPrime) console.log(number);
+  }
 }
-
 
 // Clean Code
 showPrime(20);
 
 function showPrime(limit) {
-    for (let number = 2; number <= limit; number++) 
-        if (isPrime(number)) console.log(number)
+  for (let number = 2; number <= limit; number++)
+    if (isPrime(number)) console.log(number);
 }
 
 function isPrime(number) {
-    for (let factor = 2; factor < number; factor++) 
-        if (number % factor === 0) 
-            return false;
-            
-    return true;
+  for (let factor = 2; factor < number; factor++)
+    if (number % factor === 0) return false;
+
+  return true;
 }
